66728ae51e60cbc9627745703986577d784aa68b
Abdulelah Ragih <abdulelah.ggar@gmail.com>
2023-09-06 03:32:37 +0300

    add the QueryBuilderException to become the default exception for the QueryBuilder

---
+++ src/Data/QueryBuilderException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Abdulelahragih\QueryBuilder\Data;
+
+use Exception;
+
+class QueryBuilderException extends Exception
+{
+    /**
+     * Error codes
+     * 0 - 999: Database errors
+     * 1000 - 1999: Types and Validation errors
+     * 2000 - 2999: Query and Query data errors
+     */
+    const EXECUTE_ERROR = 0;
+    const INVALID_ORDER_TYPE = 1000;
+    const INVALID_JOIN_TYPE = 1001;
+    const MISSING_TABLE = 2000;
+
+    public function __construct(int $errorCode, string $message = '')
+    {
+        parent::__construct($message);
+    }
+}
