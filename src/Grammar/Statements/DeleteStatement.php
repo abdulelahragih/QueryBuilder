4cc8c928f0b96981b5fabf45b5c5092b751c7941
Abdulelah Ragih <abdulelah.ggar@gmail.com>
2023-09-21 03:19:33 +0300

    implement delete and insert statements

---
+++ src/Grammar/Statements/DeleteStatement.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Abdulelahragih\QueryBuilder\Grammar\Statements;
+
+use Abdulelahragih\QueryBuilder\Data\QueryBuilderException;
+use Abdulelahragih\QueryBuilder\Grammar\Clauses\WhereClause;
+use Abdulelahragih\QueryBuilder\Traits\CanBuildClause;
+
+class DeleteStatement implements Statement
+{
+
+    use CanBuildClause;
+
+    /**
+     * @param string $table
+     * @param array|null $joinClause
+     * @param WhereClause|null $whereClause
+     * @param bool $forceDelete
+     */
+    public function __construct(
+        private readonly string       $table,
+        private readonly ?array       $joinClause = null,
+        private readonly ?WhereClause $whereClause = null,
+        private bool                  $forceDelete = false
+    )
+    {
+    }
+
+    /**
+     * @param bool $forceUpdate
+     */
+    public function setForceDelete(bool $forceUpdate): void
+    {
+        $this->forceDelete = $forceUpdate;
+    }
+
+    /**
+     * @throws QueryBuilderException
+     */
+    public function build(): string
+    {
+        if (!isset($this->whereClause) || empty($this->whereClause->conditionClauses->getConditions())) {
+            if (!$this->forceDelete) {
+                throw new QueryBuilderException(
+                    QueryBuilderException::DANGEROUS_QUERY,
+                    'Delete statement without where clause is not allowed. Use force(true) to force delete.');
+            }
+        }
+        return 'DELETE FROM ' . $this->table .
+            $this->buildOrEmpty($this->joinClause) .
+            $this->buildOrEmpty($this->whereClause);
+    }
+}
