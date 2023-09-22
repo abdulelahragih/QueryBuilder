0c3f65fd8086e3f044bba66dd4d2f0e88f60f37c
Abdulelah Ragih <abdulelah.ggar@gmail.com>
2023-09-23 00:21:52 +0300

    fix issue in InsertStatement builder adding parentheses around list of values

--- src/Grammar/Statements/InsertStatement.php
+++ src/Grammar/Statements/InsertStatement.php
@@ -7,23 +7,25 @@ use Abdulelahragih\QueryBuilder\Traits\CanBuildClause;
 class InsertStatement implements Statement
 {
     use CanBuildClause;
+
     public function __construct(
         private readonly string $table,
-        private readonly array $columns,
-        private readonly array $values,
+        private readonly array  $columns,
+        private readonly array  $values,
     )
     {
     }
 
     public function build(): string
     {
-        return 'INSERT INTO ' . $this->table . ' (' . implode(', ', $this->columns) . ') VALUES (' .
-            $this->buildValuesClause($this->values). ')';
+        return 'INSERT INTO ' . $this->table . ' (' . implode(', ', $this->columns) . ') VALUES ' .
+            $this->buildValuesClause($this->values);
     }
 
-    private function buildValuesClause(array $values): string {
+    private function buildValuesClause(array $values): string
+    {
         $valueClauses = [];
-        if (!empty($values) && is_array($values[0])) {
+        if (!empty($values) && is_array(reset($values))) {
             // $values is an array of arrays (multiple rows)
             foreach ($values as $row) {
                 $rowValues = implode(', ', $row);
@@ -32,7 +34,7 @@ class InsertStatement implements Statement
         } else {
             // $values is a single array (single row)
             $rowValues = implode(', ', $values);
-            $valueClauses[] =  $rowValues ;
+            $valueClauses[] = '(' . $rowValues . ')';
         }
         return implode(', ', $valueClauses);
     }
