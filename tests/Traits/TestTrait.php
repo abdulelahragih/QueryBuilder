32b8e916d9aeb467d9f5c001438f164e85bfd074
Abdulelah Ragih <abdulelah.ggar@gmail.com>
2023-09-23 00:22:26 +0300

    add tests for insert, update, and delete

--- tests/Traits/TestTrait.php
+++ tests/Traits/TestTrait.php
@@ -14,12 +14,20 @@ trait TestTrait
         $this->seedFakeData();
     }
 
-    private function seedFakeData() {
-         // create table user
-        $this->pdo->exec('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255));');
+    private function seedFakeData()
+    {
+        // create table user
+        $this->pdo->exec('
+        CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY,
+            name VARCHAR(255)
+                                         );
+');
+        // delete all data
+        $this->pdo->exec('DELETE FROM users');
         // insert data
-        $this->pdo->exec('INSERT INTO user (name) VALUES ("Sam");');
-        $this->pdo->exec('INSERT INTO user (name) VALUES ("John");');
-        $this->pdo->exec('INSERT INTO user (name) VALUES ("Jane");');
-}
+        $this->pdo->exec("INSERT INTO users (id, name) VALUES (1, 'Sam');");
+        $this->pdo->exec("INSERT INTO users (id, name) VALUES (2, 'John');");
+        $this->pdo->exec("INSERT INTO users (id, name) VALUES (3, 'Jane');");
+    }
 }
